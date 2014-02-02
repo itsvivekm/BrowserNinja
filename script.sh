1a289e63370181cd14db3ed3ae3c53709f3b30a0
kumarrus <rushab3737@gmail.com>
2014-02-02 02:29:41 -0500

    Chrome focus added manually.

--- script.sh
+++ script.sh
@@ -18,62 +18,48 @@
 #Check if Chromium is open or not
 if xwininfo -id $(xprop -root | awk '/NET_ACTIVE_WINDOW/ { print $5; exit }') | awk -F\" '/xwininfo:/ { print $2; exit }'| grep --color Chromium
 then
-	echo "found"
-else
-    if wmctrl -a Chromium # GOTO Chromium
-    then
-        echo "focused"
-    else
-        Chromium
+	echo "focused"
+#else
+#    	wmctrl -a Chromium # GOTO Chromium
+	echo "changing focus"
+	#Gesture Selection
+	if [ $1 -eq 1 ] # GOTO Previous TAB 
+	then
+	  xte 'keydown Control_L' 'key Page_Up' 'keyup Control_L'
 
-        xte 'keydown Control_L' 'key T' 'keyup Control_L'
-        xte 'str http://www.hackeracademy.org/hackathon/index.html' 'key Return'
+	elif [ $1 -eq 2 ] # GOTO Next TAB
+	then
+	  xte 'keydown Control_L' 'key Page_Down' 'keyup Control_L'
 
-        xte 'keydown Control_L' 'key T' 'keyup Control_L'
-        xte 'str http://theoatmeal.com/comics/bobcats_monday' 'key Return'
+	elif [ $1 -eq 3 ] # OPEN New TAB
+	then
+	  xte 'keydown Control_L' 'key T' 'keyup Control_L'
 
-    fi
+	elif [ $1 -eq 4 ] # CLOSE Current TAB
+	then
+	  xte 'keydown Control_L' 'key F4' 'keyup Control_L'
+
+	elif [ $1 -eq 5 ] # SCROLL Page UP
+	then
+	  for i in `seq 1 5` 
+	  do
+	    xte 'key Up'
+	    sleep 0.01
+	  done
+
+	elif [ $1 -eq 6 ] # SCROLL Page DOWN
+	then
+	  for i in `seq 1 5` 
+	  do
+	    xte 'key Down'
+	    sleep 0.01
+	  done
+
+	else
+	  xte 'keydown Control_L' 'key T' 'keyup Control_L'
+	  xte 'str www.hackeracademy.org' 'key Return'
+	  xte 'key F11'
+
+	fi
 fi
-
-
-#Gesture Selection
-if [ $1 -eq 1 ] # GOTO Previous TAB 
-then
-  xte 'keydown Control_L' 'key Page_Up' 'keyup Control_L'
-
-elif [ $1 -eq 2 ] # GOTO Next TAB
-then
-  xte 'keydown Control_L' 'key Page_Down' 'keyup Control_L'
-
-elif [ $1 -eq 3 ] # OPEN New TAB
-then
-  xte 'keydown Control_L' 'key T' 'keyup Control_L'
-
-elif [ $1 -eq 4 ] # CLOSE Current TAB
-then
-  xte 'keydown Control_L' 'key F4' 'keyup Control_L'
-
-elif [ $1 -eq 5 ] # SCROLL Page UP
-then
-  for i in `seq 1 5` 
-  do
-    xte 'key Up'
-    sleep 0.01*i
-  done
-
-elif [ $1 -eq 6 ] # SCROLL Page DOWN
-then
-  for i in `seq 1 5` 
-  do
-    xte 'key Down'
-    sleep 0.01*i
-  done
-
-else
-  xte 'keydown Control_L' 'key T' 'keyup Control_L'
-  xte 'str www.hackeracademy.org' 'key Return'
-  xte 'key F11'
-
-fi
-
 
