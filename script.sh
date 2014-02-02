20f0e2bc84cb6470b46588d471b7fbdbae44da96
kumarrus <rushab3737@gmail.com>
2014-02-02 12:13:34 -0500

    Added notifications

--- script.sh
+++ script.sh
@@ -14,7 +14,6 @@
 # 7. C3-Hack
 #
 
-
 #Check if Chromium is open or not
 if xwininfo -id $(xprop -root | awk '/NET_ACTIVE_WINDOW/ { print $5; exit }') | awk -F\" '/xwininfo:/ { print $2; exit }'| grep --color Chromium
 then
@@ -25,22 +24,27 @@ then
 	#Gesture Selection
 	if [ $1 -eq 1 ] # GOTO Previous TAB 
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Katana Chop Right"
 	  xte 'keydown Control_L' 'key Page_Up' 'keyup Control_L'
 
 	elif [ $1 -eq 2 ] # GOTO Next TAB
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Katana Chop Left"
 	  xte 'keydown Control_L' 'key Page_Down' 'keyup Control_L'
 
 	elif [ $1 -eq 3 ] # OPEN New TAB
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Ninja Mode ON"
 	  xte 'keydown Control_L' 'key T' 'keyup Control_L'
 
 	elif [ $1 -eq 4 ] # CLOSE Current TAB
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Ninja Mode ON"
 	  xte 'keydown Control_L' 'key F4' 'keyup Control_L'
 
 	elif [ $1 -eq 5 ] # SCROLL Page UP
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Peek Up"
 	  for i in `seq 1 5` 
 	  do
 	    xte 'key Up'
@@ -49,6 +53,7 @@ then
 
 	elif [ $1 -eq 6 ] # SCROLL Page DOWN
 	then
+	  notify-send -i /home/kumarrus/github/Wave/images/ninja3.png -t 1000 -a "Browser Ninja" "Peek Down"
 	  for i in `seq 1 5` 
 	  do
 	    xte 'key Down'
